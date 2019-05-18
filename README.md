d2891c4eea4b049fa05af64a7f0d5a54c5c12909
lzcdev <lzc2134@gmail.com>
2019-05-18 16:12:17 +0800

    增加MongoDB数据库

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 
-Express 搭建的 Todo List 小项目
+Todo List 小项目, Node + Express + MongoDB
 
 ## 使用
 
 ```
 npm install
 ```
+
+启动
+
+```
+node app.js 
+```
+
+## 说明
+
+MongoDB用的是在线的数据库
+[https://console.scalegrid.io/](https://console.scalegrid.io/)
+有一个月的免费试用期
+
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,30 +1,46 @@
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+var mongoose = require('mongoose');
 
-var data = [
-  { item: 'get milk' },
-  { item: 'walk dog' },
-  { item: 'kick some coding ass' },
-];
+mongoose.connect('mongodb://admin:rZ53QHKeJz0LaspO@SG-TodoList-21241.servers.mongodirector.com:48561,SG-TodoList-21242.servers.mongodirector.com:48561,SG-TodoList-21243.servers.mongodirector.com:48561/admin?replicaSet=RS-TodoList-0&ssl=true')
 
-module.exports = (app => {
-  app.get('/todo', (req, res) => {
-    res.render('todo', { todos: data })
-  });
-
-  app.post('/todo', urlencodedParser, (req, res) => {
-    data.push(req.body);
-    res.json(data);
-  });
-
-  app.delete('/todo/:item', (req, res) => {
-    data = data.filter(todo => {
-      return todo.item.replace(/ /g, "-") !== req.params.item;
-    });
-    res.json(data);
-  });
-
-
-
+var todoSchema = new mongoose.Schema({
+  item: String
 });
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err) {
+//   if (err) throw err;
+//   console.log('item saved');
+// });
+//
+// var data = [ {item: 'get milk'}, {item: "walk dog"}, {item: 'kick some coding ass'} ];;
+
+module.exports = function (app) {
+  app.get('/todo', function (req, res) {
+    Todo.find({}, function (err, data) {
+      if (err) throw err;
+      res.render('todo', { todos: data });
+    });
+  });
+
+  app.post('/todo', urlencodedParser, function (req, res) {
+    var itemOne = Todo(req.body).save(function (err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
+
+  app.delete('/todo/:item', function (req, res) {
+    // data = data.filter(function(todo) {
+    //   return todo.item.replace(/ /g, "-") !== req.params.item;
+    // });
+    Todo.find({ item: req.params.item.replace(/-/g, " ") }).remove(function (err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
+}
