8030b3bfde1fa94bdeea3c52994c4c42f215297b
lzcdev <lzc2134@gmail.com>
2019-05-18 15:31:16 +0800

    增加删除功能

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,14 +1,28 @@
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+var data = [
+  { item: 'get milk' },
+  { item: 'walk dog' },
+  { item: 'kick some coding ass' },
+];
+
 module.exports = (app => {
   app.get('/todo', (req, res) => {
-    res.render('todo')
+    res.render('todo', { todos: data })
   });
 
-  app.post('/todo', (req, res) => {
-
+  app.post('/todo', urlencodedParser, (req, res) => {
+    data.push(req.body);
+    res.json(data);
   });
 
-  app.delete('/todo', (req, res) => {
-
+  app.delete('/todo/:item', (req, res) => {
+    data = data.filter(todo => {
+      return todo.item.replace(/ /g, "-") !== req.params.item;
+    });
+    res.json(data);
   });
 
 
